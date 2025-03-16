98475b8e9569c542ecab0241231fafdb93e02319
Phuong Bui <buiphuongaptutc@gmail.com>
2025-03-16 22:44:31 +0700

    refactor

--- src/test/java/lecture/oop/OOPTest.java
+++ src/test/java/lecture/oop/OOPTest.java
@@ -5,7 +5,7 @@ import lecture.oop.factory.service.MockServiceFactory;
 import lecture.oop.service.OrderService;
 import org.junit.Test;
 
-public class MainTest {
+public class OOPTest {
     @Test
     public void processOrder() {
         ServiceFactory serviceFactory = new MockServiceFactory();
